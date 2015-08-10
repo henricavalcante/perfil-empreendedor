3232399d09db64eda6e61749e20701240fe68189
Henri Cavalcante <hnri_mxel@hotmail.com>
2015-08-09 23:26:57 -0300

    webpacked

--- bundle.js
+++ bundle.js
@@ -51,49 +51,195 @@
 	var ThemeManager = new mui.Styles.ThemeManager();
 	var RadioButtonGroup = mui.RadioButtonGroup;
 	var RadioButton = mui.RadioButton;
+	var CardHeader = mui.CardReader;
+	var CardText = mui.CardText;
+	var CardMedia = mui.CardMedia;
+	var CardTitle = mui.CardTitle;
+	var CardActions = mui.CardActions;
+	var Card = mui.Card;
+	var Avatar = mui.Avater;
+	var List = mui.List;
+	var ListItem = mui.ListItem;
+	var ListDivider = mui.ListDivider;
+	var FlatButton = mui.FlatButton;
+	var Colors = mui.Styles.Colors;
+	var AppCanvas = mui.AppCanvas;
+	var Paper = mui.Paper;
+	var questoes = __webpack_require__(314);
 
 	var PerfilEmpreendedor = React.createClass({
 	  displayName: 'PerfilEmpreendedor',
 
-	  render: function render() {
-	    return React.createElement(Alternativa, { id: "teste", questionId: "teste2", description: "Alternativa 01" });
-	  }
-	});
+	  childContextTypes: {
+	    muiTheme: React.PropTypes.object
+	  },
 
-	var Alternativa = React.createClass({
-	  displayName: 'Alternativa',
+	  getChildContext: function getChildContext() {
+	    return {
+	      muiTheme: ThemeManager.getCurrentTheme()
+	    };
+	  },
 
+	  componentWillMount: function componentWillMount() {
+	    ThemeManager.setPalette({
+	      accent1Color: Colors.deepOrange500
+	    });
+	  },
 	  render: function render() {
 	    return React.createElement(
-	      'div',
+	      AppCanvas,
 	      null,
-	      React.createElement('input', { ref: "{ this.props.id }", type: "radio", name: "{ this.props.questionId }" }),
 	      React.createElement(
-	        RadioButtonGroup,
-	        { name: "shipSpeed", defaultSelected: "not_light" },
-	        React.createElement(RadioButton, {
-	          value: "light",
-	          label: "prepare for light speed",
-	          style: { marginBottom: 16 } }),
-	        React.createElement(RadioButton, {
-	          value: "not_light",
-	          label: "light speed too slow",
-	          style: { marginBottom: 16 } }),
-	        React.createElement(RadioButton, {
-	          value: "ludicrous",
-	          label: "go to ludicrous speed",
-	          style: { marginBottom: 16 },
-	          disabled: true })
-	      ),
-	      React.createElement(
-	        'label',
-	        { 'for': "{ this.props.id }" },
-	        this.props.description
+	        Card,
+	        null,
+	        React.createElement(
+	          CardMedia,
+	          { overlay: React.createElement(CardTitle, {
+	              title: "Perfil do Empreendedor",
+	              subtitle: "Qual o seu perfil de empreendedor? Descubra agora mesmo." }) },
+	          React.createElement('img', { src: "http://lorempixel.com/600/337/business/" })
+	        ),
+	        React.createElement(
+	          Paper,
+	          { style: { margin: 50, padding: 50 } },
+	          React.createElement(
+	            H3,
+	            null,
+	            ' Apresentação '
+	          ),
+	          React.createElement(
+	            'p',
+	            null,
+	            ' Esta ferramenta visa realizar uma análise do seu perfil empreendedor através de questionamentos, resultando em um autoconhecimento de sua vocação empreendedora, seja empregado ou dono do próprio negócio.'
+	          )
+	        ),
+	        React.createElement(
+	          Paper,
+	          { style: { margin: 50, padding: 50 } },
+	          React.createElement(
+	            H3,
+	            null,
+	            ' Instruções '
+	          ),
+	          React.createElement(
+	            'p',
+	            null,
+	            'O principal objetivo deste questionário, é ajudá-lo a intensificar o seu autoconhecimento.  Não há respostas certas ou erradas. Mas elas, em seu conjunto apontam tendências que podem contribuir para seu aperfeiçoamento como pessoa e profissional. Ninguém é polivalente em todos os campos do conhecimento e do trabalho.'
+	          ),
+	          React.createElement(
+	            'p',
+	            null,
+	            'Responda por isso as perguntas da forma crítica e sincera.'
+	          ),
+	          React.createElement(
+	            'p',
+	            null,
+	            'Leia, portanto, as 55 questões e faça a escolha da opção de 1 a 5 que mais se aplica a você de acordo com os conceitos da tabela abaixo, e marque um “X” no número correspondente à pontuação escolhida, nas colunas à direita de cada uma das afirmações.'
+	          ),
+	          React.createElement(
+	            'ol',
+	            null,
+	            React.createElement(
+	              'li',
+	              null,
+	              'Nunca'
+	            ),
+	            React.createElement(
+	              'li',
+	              null,
+	              'Raramente'
+	            ),
+	            React.createElement(
+	              'li',
+	              null,
+	              'Algumas vezes'
+	            ),
+	            React.createElement(
+	              'li',
+	              null,
+	              'Geralmente'
+	            ),
+	            React.createElement(
+	              'li',
+	              null,
+	              'Sempre'
+	            )
+	          ),
+	          React.createElement(
+	            'p',
+	            null,
+	            'Mesmo que as afirmativas pareçam similares, elas não são réplicas uma das outras, pois procuram sempre focalizar algum aspecto diferente das demais.'
+	          )
+	        ),
+	        React.createElement(
+	          Paper,
+	          { style: { margin: 50, padding: 50 } },
+	          React.createElement(
+	            H3,
+	            null,
+	            'Questionário'
+	          ),
+	          React.createElement(
+	            List,
+	            null,
+	            this.props.questoes.map(function (questao) {
+	              return [React.createElement(Pergunta, { questao: questao }), React.createElement(ListDivider, null)];
+	            })
+	          ),
+	          React.createElement(FlatButton, { label: "Finalizar" })
+	        )
 	      )
 	    );
 	  }
 	});
-	React.render(React.createElement(PerfilEmpreendedor, null), document.body);
+
+	var H3 = React.createClass({
+	  displayName: 'H3',
+
+	  render: function render() {
+	    return React.createElement(
+	      'h3',
+	      { style: { fontSize: '20px',
+	          lineHeight: '28px',
+	          paddingBottom: '22px',
+	          letterSpacing: 0,
+	          fontWeight: 500,
+	          boxSizing: 'border-box' } },
+	      this.props.children
+	    );
+	  }
+	});
+
+	var Pergunta = React.createClass({
+	  displayName: 'Pergunta',
+
+	  render: function render() {
+	    return React.createElement(
+	      ListItem,
+	      {
+	        leftIcon: React.createElement(
+	          'span',
+	          null,
+	          this.props.questao.id
+	        ) },
+	      React.createElement(
+	        'p',
+	        null,
+	        this.props.questao.pergunta
+	      ),
+	      React.createElement(
+	        RadioButtonGroup,
+	        { name: this.props.questao.id },
+	        ["Nunca", "Raramente", "Algumas vezes", "Geralmente", "Sempre"].map(function (index) {
+	          return React.createElement(RadioButton, { value: { index: index },
+	            label: index });
+	        })
+	      )
+	    );
+	  }
+	});
+
+	React.render(React.createElement(PerfilEmpreendedor, { questoes: questoes }), document.body);
 
 /***/ },
 /* 1 */
@@ -40764,6 +40910,230 @@
 	  UniqueId: __webpack_require__(229),
 	  Styles: __webpack_require__(185)
 	};
+
+/***/ },
+/* 314 */
+/***/ function(module, exports) {
+
+	module.exports = [
+	  {
+	    "id": 1,
+	    "pergunta": "Em novas situações, quando comparadas as anteriores, procuro atingir resultados melhores do que os obtidos naquelas."
+	  },
+	  {
+	    "id": 2,
+	    "pergunta": "Não tenho problema algum em insistir e até me sacrificar para conseguir o que pretendo."
+	  },
+	  {
+	    "id": 3,
+	    "pergunta": "Se assumi um compromisso, é porque vou cumprir."
+	  },
+	  {
+	    "id": 4,
+	    "pergunta": "Desenvolvo procedimentos para atividades e estimulo os outros a fazerem o mesmo, para atingirmos o nível de qualidade desejado."
+	  },
+	  {
+	    "id": 5,
+	    "pergunta": "Acompanho o que vai acontecendo e ajusto o que foi planejado de acordo com as novas medições e circunstâncias."
+	  },
+	  {
+	    "id": 6,
+	    "pergunta": "Sei onde quero chegar com a minha vida a longo prazo e tenho um plano para isso."
+	  },
+	  {
+	    "id": 7,
+	    "pergunta": "Lanço mão de variadas fontes de informação para executar tarefas e projetos."
+	  },
+	  {
+	    "id": 8,
+	    "pergunta": "Se o método de resolução de um problema não funciona, lanço mão de outros."
+	  },
+	  {
+	    "id": 9,
+	    "pergunta": "Procuro localizar os benefícios para todos os envolvidos em um problema, para conseguir convencê-los."
+	  },
+	  {
+	    "id": 10,
+	    "pergunta": "Aposto que alcançarei sucesso, mesmo em projetos difíceis e desafiadores."
+	  },
+	  {
+	    "id": 11,
+	    "pergunta": "Ouço sempre qualquer pessoa com muita atenção."
+	  },
+	  {
+	    "id": 12,
+	    "pergunta": "Empenho-me em cumprir tudo o que se deve ser feito, antecipando-me às circunstâncias."
+	  },
+	  {
+	    "id": 13,
+	    "pergunta": "Busco formas de contornar obstáculos e dificuldades, mas sem perder o foco."
+	  },
+	  {
+	    "id": 14,
+	    "pergunta": "Desdobro-me para cumprir o prazo de entrega prometido e até faço a parte dos outros."
+	  },
+	  {
+	    "id": 15,
+	    "pergunta": "Registro todos os gastos e os consulto para ter base para novos projetos e planos."
+	  },
+	  {
+	    "id": 16,
+	    "pergunta": "Analiso todos os aspectos de um novo projeto e somente me envolvo depois de fazer o que podia para garantir o seu êxito."
+	  },
+	  {
+	    "id": 17,
+	    "pergunta": "Não perco tempo preocupando-me com o futuro de minha vida."
+	  },
+	  {
+	    "id": 18,
+	    "pergunta": "Questiono bastante para definir exatamente o que querem os clientes antes de sair executando um trabalho ou uma encomenda."
+	  },
+	  {
+	    "id": 19,
+	    "pergunta": "Procuro subdividir um grande trabalho ou projeto em atividades menores, definindo prazos."
+	  },
+	  {
+	    "id": 20,
+	    "pergunta": "Não antecipo argumentos e formas de influenciar outras pessoas."
+	  },
+	  {
+	    "id": 21,
+	    "pergunta": "Ante firme discordância dos meus pontos de vista, procuro adequá-las."
+	  },
+	  {
+	    "id": 22,
+	    "pergunta": "Se não obtenho o que desejo, fico irritado."
+	  },
+	  {
+	    "id": 23,
+	    "pergunta": "Mexo sim em time que está ganhando: sempre há diferentes maneiras de realizar o que já fizemos antes."
+	  },
+	  {
+	    "id": 24,
+	    "pergunta": "Dedico o tempo que for necessário para resolver um problema difícil."
+	  },
+	  {
+	    "id": 25,
+	    "pergunta": "Faço o que estiver ao meu alcance para satisfazer a quem contratou o trabalho."
+	  },
+	  {
+	    "id": 27,
+	    "pergunta": "Evito situações em que percebo que o resultado final fugiria ao meu controle."
+	  },
+	  {
+	    "id": 28,
+	    "pergunta": "O futuro é pensado antes e eu gosto de pensar e escolher marcos significativos para mim."
+	  },
+	  {
+	    "id": 29,
+	    "pergunta": "Como o mais importante é decidir e agir rápido, não perco tempo buscando atualizar as informações."
+	  },
+	  {
+	    "id": 30,
+	    "pergunta": "Estou sempre alerta para as possibilidades e procuro fazer novos negócios ou oferecer novos serviços."
+	  },
+	  {
+	    "id": 31,
+	    "pergunta": "Consigo conquistar o aval e a concordância dos outros para os meus pontos de vista ou propostas."
+	  },
+	  {
+	    "id": 32,
+	    "pergunta": "Tenho convicção de determinado direcionamento de minhas ações; não serão normas e expectativas dos outros que me deterão."
+	  },
+	  {
+	    "id": 33,
+	    "pergunta": "Já sofri insucessos em minha vida."
+	  },
+	  {
+	    "id": 34,
+	    "pergunta": "Prefiro trabalhar em assuntos ou projetos que conheço perfeitamente e nos quais me sinto seguro."
+	  },
+	  {
+	    "id": 35,
+	    "pergunta": "Perante dificuldades, prefiro passar para outras tarefas ou trabalhos."
+	  },
+	  {
+	    "id": 36,
+	    "pergunta": "Finalizo meus trabalhos ou projetos no prazo acertado."
+	  },
+	  {
+	    "id": 37,
+	    "pergunta": "O tempo é precioso e fico irritado se o perco."
+	  },
+	  {
+	    "id": 38,
+	    "pergunta": "Disponho-me facilmente a seguir empreitadas arriscadas."
+	  },
+	  {
+	    "id": 39,
+	    "pergunta": "Devoto a mesma energia para concretizar minhas metas semanais, mensais ou anuais."
+	  },
+	  {
+	    "id": 40,
+	    "pergunta": "Costumo consultar as pessoas experientes ou especialistas na área ou ramo de negócio em que estou atuando."
+	  },
+	  {
+	    "id": 41,
+	    "pergunta": "Sigo a maré, isto é, lido com os problemas à medida que surgem."
+	  },
+	  {
+	    "id": 42,
+	    "pergunta": "Consigo fazer com que as pessoas mudem de opinião, mesmo que elas tenham a princípio, firmeza em suas posições."
+	  },
+	  {
+	    "id": 43,
+	    "pergunta": "Mesmo quando confrontado com oposição, não altero a minha decisão e firmeza."
+	  },
+	  {
+	    "id": 44,
+	    "pergunta": "Já passei por situação em que obtive vantagem de outras pessoas."
+	  },
+	  {
+	    "id": 45,
+	    "pergunta": "Não fico esperando que me peçam para fazer as coisas, saio na frente e faço antes pergunta: '"
+	  },
+	  {
+	    "id": 46,
+	    "pergunta": "Avalio bem todos os prós e contras das diferentes possibilidades, antes de executar um trabalho ou projeto."
+	  },
+	  {
+	    "id": 47,
+	    "pergunta": "Não coloco prazos de entrega de trabalhos acima de minha vida pessoal e familiar."
+	  },
+	  {
+	    "id": 48,
+	    "pergunta": "No trabalho minha produtividade tende a ser superior à das outras pessoas, pois sempre encontro formas de acelerar  ou fazer melhor."
+	  },
+	  {
+	    "id": 49,
+	    "pergunta": "Não inicio trabalho ou projetos antes de me assegurar de que vale a pena arriscar."
+	  },
+	  {
+	    "id": 50,
+	    "pergunta": "Quanto mais definir o que quero atingir, maior probabilidade de sucesso terei na minha vida."
+	  },
+	  {
+	    "id": 51,
+	    "pergunta": "Reúno todas as informações possíveis antes de executar um projeto."
+	  },
+	  {
+	    "id": 52,
+	    "pergunta": "Procuro atender todas as possibilidades e preparo alternativas para elas."
+	  },
+	  {
+	    "id": 53,
+	    "pergunta": "Para atingir minhas metas, envolvo pessoas que podem influir para que eu alcance os meus propósitos."
+	  },
+	  {
+	    "id": 54,
+	    "pergunta": "Tenho confiança de que me sairei bem em qualquer atitude que me interesse fazer."
+	  },
+	  {
+	    "id": 55,
+	    "pergunta": "Sinto-me à vontade para reconhecer meu desconhecimento sobre algum assunto."
+	  }
+	];
+
 
 /***/ }
 /******/ ]);
